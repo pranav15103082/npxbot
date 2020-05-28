4456722d2e08df1581b645a08ffded9e5263ac7c
Pranav <30601604+pranav15103082@users.noreply.github.com>
2020-05-29 02:55:44 +0530

    Delete cred.py

--- cred.py
+++
@@ -1,9 +0,0 @@
-import urllib
-import pymongo
-from pymongo import MongoClient
-
-mango_url = "mongodb+srv://npranavx:" + urllib.parse.quote_plus("Pr@nav123") + "@npxbot-bfw7s.mongodb.net/test"
-cluster = MongoClient(mango_url)
-db = cluster["npx-bot"]
-collection = db["search_results"]
-Token = 'NzE1Mjc4NjEwOTk0NDMwMDEz.Xs65zg.W6WLVTM7qgVJY80EYG1uu3A06lA'
